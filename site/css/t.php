132b92acb24db9fa7213808cca47ca9e01db3156
Max van Gorp <534253@student.glu.nl>
2020-05-15 09:31:15 +0200

    first commit

---
+++ site/css/t.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+class Student {
+    var $voornaam,$achternaam,$woonplaats; 
+    // var $woonplaats;
+    // var 
+    function GeefCijfer(){
+        echo test;
+    }
+
+}
+$student1 = new Student; 
+$student1->$voornaam = "Max";
+$student2->$voornaam = "Robin";
+
+?>
